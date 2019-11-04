d2df6acff89432485df3db33e54b020ea6a29a7d
Unknown <rispir45@gmail.com>
2019-11-04 01:01:18 -0500

    Added main.js and call to SearchPage

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+/* Created By: Patrick Conboy
+ * Created On: November 4, 2019
+ * Description: This file handle compiling all the different webpages
+ * of the AniTracker website.
+*/
+
+import React, { Component} from 'react'
+
+import SearchPage from './SearchPage'
+
+class Main extends Component {
+  render() {
+    return (
+      <SearchPage />
+    )
+  }
+}
+
+export default Main
