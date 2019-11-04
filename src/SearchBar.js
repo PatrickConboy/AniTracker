15e96e347912a3fa33543fe1ee4cb5454d6decfe
Unknown <rispir45@gmail.com>
2019-11-04 01:01:43 -0500

    Added SearchBar.js and SearchBar.test.js and basic setup

---
+++ src/SearchBar.js
@@ -0,0 +1,23 @@
+/* Created By: Patrick Conboy
+ * Created On: November 4, 2019
+ * Description: This file handles the search bar functionality for the 
+ * search page of AniTracker.
+*/
+
+import React, { Component } from 'react'
+
+// This class is the search bar for users to search the database.
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Search Bar
+        </p>
+        <input></input>
+      </div>
+    )
+  }
+}
+
+export default SearchBar
