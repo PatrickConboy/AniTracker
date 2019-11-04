6007a9c7c16c693b1fe56c24ef9d225fa91ff885
Unknown <rispir45@gmail.com>
2019-11-04 01:00:35 -0500

    Got basic setup done for app.js, app.css. and app.test.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+/* Created By: Patrick Conboy
+ * Created On: November 1, 2019
+ * Description: This is the main App file for the AniTracker website.
+ * It simply calls Main and handles any basic constructor stuff.
+*/
+
+import React, { Component } from 'react'
+
+import Main from './Main'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Constructor currently does nothing, but
+  // will need later for login functionality.
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Main/>
+      </div>
+    )
+  }
 }
 
 export default App;
