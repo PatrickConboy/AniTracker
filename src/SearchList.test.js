138f39c5b72c516184c77bf83849f2da5d995347
Unknown <rispir45@gmail.com>
2019-11-18 01:58:52 -0500

    Added List component in SearchList and got it working.

--- src/SearchList.test.js
+++ src/SearchList.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SearchList from './SearchList'
+import List from './SearchList'
+import { exportAllDeclaration } from '@babel/types'
+import { AssertionError } from 'assert'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<SearchList />, div)
   ReactDOM.unmountComponentAtNode(div)
+})
+
+it('list has correct number of anime in it', () => {
+  assert.equal(1, 1)
 })
