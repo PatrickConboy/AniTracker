ff780c43f10cbe804939c08512ac65d7cd674ed8
Unknown <rispir45@gmail.com>
2019-11-30 00:53:20 -0500

    Finished adding firebase context

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import App from './App'
 import Firebase, { FirebaseContext } from './components/Firebase'
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={new Firebase()}>
+  <FirebaseContext.Provider value={new Firebase()}>
       <App />
-    </FirebaseContext.Provider>, 
-    document.getElementById('root')
-  )
+  </FirebaseContext.Provider>, 
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
