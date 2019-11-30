3feed3fb107f4704c874b05719305f2a5bfcd255
Unknown <rispir45@gmail.com>
2019-11-30 00:53:20 -0500

    Finished adding firebase context

---
+++ src/components/Firebase/Firebase.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyD-Qrqk60VFoRrWzgN7yBIaqPcWNOOlbRo",
+  authDomain: "anitracker-9f73f.firebaseapp.com",
+  databaseURL: "https://anitracker-9f73f.firebaseio.com",
+  projectId: "anitracker-9f73f",
+  storageBucket: "anitracker-9f73f.appspot.com",
+  messagingSenderId: "226302437942",
+  appId: "1:226302437942:web:e71e5b6b9e2f9682580697",
+  measurementId: "G-JH7JQ601CY"
+};
