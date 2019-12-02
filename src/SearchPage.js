e3895dfe421ef97c1b3004022850bd477efeabf3
Unknown <rispir45@gmail.com>
2019-12-01 20:25:29 -0500

    Fixed some documentation for header comments.

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,7 +1,5 @@
 /* Author: Patrick Conboy
-
  * Date Created: 11/4/2019
- 
  * Description: This page allows the user to search our database list of anime.
  * The FilterSearchList provides most of the actual functionality code.
  */
@@ -13,6 +11,11 @@ import firebase from './components/Firebase/firebase'
 class SearchPage extends Component {
   constructor(props) {
     super(props)
+
+    const db = firebase.firestore()
+    let animeList = db.collection("anime").get()
+    console.log(animeList)
+
 
     // TODO: this list should be populated by database from Firebase
     this.state = {
