548b080e32b7afa1d63d3f2e6e2cbbcf935bc16c
Unknown <rispir45@gmail.com>
2019-12-27 17:47:42 -0500

    Refactored and added snapshot for App
    #16

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('App Tests', () => {
+  it('app renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+
+  test('snapshot of app hasn\'t changed', () => {
+    const component = renderer.create(<App />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
