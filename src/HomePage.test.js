513be3c45afbeb6541a4f9efa7ebf77a95f8c295
Unknown <rispir45@gmail.com>
2019-12-27 17:50:15 -0500

    Refactored and added snapshot for HomePage
    #16

--- src/HomePage.test.js
+++ src/HomePage.test.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+
 import HomePage from './HomePage'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<HomePage />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('HomePage Tests', () => {
+  it('home page renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<HomePage />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+
+  test('snapshot of home page hasn\'t changed', () => {
+    const component = renderer.create(<HomePage />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
