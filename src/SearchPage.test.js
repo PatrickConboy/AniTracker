7e0432dc4c0818800cbcdc80db597dde3ba758b1
Unknown <rispir45@gmail.com>
2019-12-27 17:36:21 -0500

    Figured out how to create snapshot test for Search Page
    #12

--- src/SearchPage.test.js
+++ src/SearchPage.test.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+
 import SearchPage from './SearchPage'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<SearchPage />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
+describe('SearchPage Tests', () => {
+  it('search page renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<SearchPage />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
 
-it('My Test Case', () => {
+
+  test('snapshot of search page hasn\'t changed', () => {
+    const component = renderer.create(<SearchPage />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  });
+
+
   let searchList = new SearchPage("props")
   let animeList = searchList.state["animeList"]
   expect(animeList.length).toEqual(4)
+})
 })
