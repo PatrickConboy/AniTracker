a9c7012b3b57595a4eca1d45ce239ce7b9648904
Unknown <rispir45@gmail.com>
2019-12-27 17:49:09 -0500

    Refactored and added snapshot for NavBar
    #16

--- src/NavBar.test.js
+++ src/NavBar.test.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+
 import NavBar from './NavBar'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<NavBar />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('NavBar Tests', () => {
+  it('nav bar renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<NavBar />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+
+  test('snapshot of nav bar hasn\'t changed', () => {
+    const component = renderer.create(<NavBar />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
