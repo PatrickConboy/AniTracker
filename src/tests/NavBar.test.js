340dca1cf5cfe10ae36cf9180ecb3155d6a00202
Unknown <rispir45@gmail.com>
2020-01-01 20:09:40 -0500

    Added basic Enzyme test for NavBar
    #12

--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
+import { shallow, mount, render } from '../enzyme'
 
 import NavBar from '../NavBar'
 
@@ -11,6 +12,12 @@ describe('NavBar Tests', () => {
     ReactDOM.unmountComponentAtNode(div)
   })
 
+  test('nav bar renders correctly', () => {
+    const wrapper = shallow(<NavBar />)
+
+    expect(wrapper.find('#NavBar')).toBeDefined()
+    expect(wrapper.find('#navLinks')).toBeDefined()
+  })
 
   test('snapshot of nav bar hasn\'t changed', () => {
     const component = renderer.create(<NavBar />)
