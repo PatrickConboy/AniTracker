1dbc2a7cf55d28618acae71f27954b69c6accece
Unknown <rispir45@gmail.com>
2020-01-21 23:56:18 -0500

    Added button to anime page that does nothing so far
    #31

--- src/AnimePage.js
+++ src/AnimePage.js
@@ -48,32 +48,41 @@ class AnimePage extends Component {
         animeProducers: animeData['producers'],
         animeLicensors: animeData['licensors'],
         animeStudios: animeData['studios'],
-        sourceMaterial:animeData['source'],
+        sourceMaterial: animeData['source'],
         animeGenres: animeData['genres'],
         animeDuration: animeData['duration'],
         animeRating: animeData['rating'],
       })
     })
   }
- 
+
   render() {
     return (
-      <div className="animeData">
-        <h2>{this.state.animeName}</h2>
-        <h3>Synopsis: {this.state.animeSynopsis}</h3>
-        <h3>Background: {this.state.animeBackground}</h3>
-        <h3>Type: {this.state.typeOfAnime}</h3>
-        <h3>Episodes: {this.state.numberOfEpisodes}</h3>
-        <h3>Status: {this.state.animeStatus}</h3>
-        <h3>Aired: {this.state.dateAired}</h3>
-        <h3>Premiered: {this.state.seasonPremiered}</h3>
-        <h3>Producers: {this.state.animeProducers}</h3>
-        <h3>Licensors: {this.state.animeLicensors}</h3>
-        <h3>Studios: {this.state.animeStudios}</h3>
-        <h3>Source: {this.state.sourceMaterial}</h3>
-        <h3>Genres: {this.state.animeGenres}</h3>
-        <h3>Duration: {this.state.animeDuration}</h3>
-        <h3>Rating: {this.state.animeRating}</h3>
+      <div className="animeInfo">
+        <div className="nameSpace">
+          <h2>{this.state.animeName}</h2>
+          <button type="button" className="btn">Add to list</button>
+        </div>
+
+        <div className="animeBlurbs">
+          <h3>Synopsis: {this.state.animeSynopsis}</h3>
+          <h3>Background: {this.state.animeBackground}</h3>
+        </div>
+
+        <div className="animeDataColumn">
+          <h3>Type: {this.state.typeOfAnime}</h3>
+          <h3>Episodes: {this.state.numberOfEpisodes}</h3>
+          <h3>Status: {this.state.animeStatus}</h3>
+          <h3>Aired: {this.state.dateAired}</h3>
+          <h3>Premiered: {this.state.seasonPremiered}</h3>
+          <h3>Producers: {this.state.animeProducers}</h3>
+          <h3>Licensors: {this.state.animeLicensors}</h3>
+          <h3>Studios: {this.state.animeStudios}</h3>
+          <h3>Source: {this.state.sourceMaterial}</h3>
+          <h3>Genres: {this.state.animeGenres}</h3>
+          <h3>Duration: {this.state.animeDuration}</h3>
+          <h3>Rating: {this.state.animeRating}</h3>
+        </div>
       </div>
     )
   }
