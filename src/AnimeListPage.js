b8369f7de8067744cb638ecb46f8b2440c9aab39
Unknown <rispir45@gmail.com>
2020-01-25 19:44:18 -0500

    Personal anime list showing and can click each item to visit page
    #32

--- src/AnimeListPage.js
+++ src/AnimeListPage.js
@@ -5,12 +5,68 @@
  */
 
 import React, { Component } from 'react'
+import firebase from './components/firebase'
+
+import AnimePage from './AnimePage'
 
 class AnimeListPage extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      personalList: [],
+      isPersonalList: true,
+      isAnimePage: false,
+      animeName: ""
+    }
+
+    this.showAnimePage = this.showAnimePage.bind(this)
+  }
+
+  async componentDidMount() {
+    const db = await firebase.firestore()
+    const personalListFromDb = []
+    await db.collection("personalList").get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+        let anime = doc.data()
+        let animeName = anime['name']
+        personalListFromDb.push(animeName)
+      })
+    })
+
+    this.setState({ personalList: personalListFromDb })
+  }
+
+  showAnimePage(anime) {
+    this.setState({
+      isPersonalList: false,
+      isAnimePage: true,
+      animeName: anime
+    })
+  }
+
+  animePage() {
     return (
-      <div className="AnimeListPage">
-        <h3>Welcome to your personal anime list!</h3>
+      <div>
+        <AnimePage animeName={this.state.animeName} />
+      </div>
+    )
+  }
+
+  render() {
+    var personalListPage = (
+      <div>
+        <ul>
+          {this.state.personalList.map(item => (<li className={item} onClick={this.showAnimePage.bind(this, item)} key={item}>{item}</li>))}
+        </ul>
+      </div>
+    )
+
+    // This is saying that if isPersonalList is true, render that component. 
+    // Otherwise, render isAnimePage's component.
+    return (
+      <div>
+        { this.state.isPersonalList ? personalListPage : null }
+        { this.state.isAnimePage ? this.animePage(): null }
       </div>
     )
   }
